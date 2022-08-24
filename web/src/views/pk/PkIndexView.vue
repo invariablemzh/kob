587244b5a41d7d750f0be953b2496e14696874ea
invariablemzh <invariablemzh@gmail.com>
2022-08-24 09:29:22 +0800

    实现了导航栏 + 对战页面的地图和障碍物

---
+++ web/src/views/pk/PkIndexView.vue
@@ -0,0 +1,21 @@
+<template>
+    <play-ground></play-ground>
+</template>
+
+
+<script>
+import PlayGround from '../../components/PlayGround.vue'
+
+
+export default{
+    components:{
+        PlayGround
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
